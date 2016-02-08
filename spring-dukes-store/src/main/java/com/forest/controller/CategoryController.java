bdd69c8d79d6c366e285fedda08f4fb9eb2a4d22
Andrej Burenin <andrejs.burenins@dukascopy.com>
2016-02-08 09:07:02 +0200

    weekend updates

--- spring-dukes-store/src/main/java/com/forest/controller/CategoryController.java
+++ spring-dukes-store/src/main/java/com/forest/controller/CategoryController.java
@@ -1,15 +1,20 @@
 package com.forest.controller;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.forest.controller.support.MessageHelper;
 import com.forest.entity.Category;
 import com.forest.service.ICategoryService;
 
@@ -87,4 +92,23 @@ public class CategoryController {
         return "/admin/category/create";
     }
 	
+	@RequestMapping(value = "/admin/category/create", method = RequestMethod.POST)
+    public String adminCategoryCreatePost(@Valid Category category, BindingResult result, RedirectAttributes ra) {
+		if (result.hasErrors()) {
+            return "/admin/category/create";
+        } else {
+        	category = categoryService.save(category);
+        	
+        	MessageHelper.addSuccessAttribute(ra, "CategoryCreated");
+            return "redirect:/admin/category/create";
+        }
+    }
+	
+	
+	@RequestMapping(value = "/admin/category/prepareList", method = RequestMethod.GET)
+    public String adminCategoryPrepareList(Model model) {
+        model.addAttribute("items", categoryService.getItems());
+        model.addAttribute("pagination", categoryService.getPagination());
+        return "/admin/category/list";
+    }
 }
