8d87c36768f8d252a35f08bfe161f10c11972721
Andrej Burenin <andrejs.burenins@dukascopy.com>
2016-02-10 17:38:12 +0200

    payment service

--- spring-dukes-payment/src/main/java/com/forest/payment/services/PaymentApplication.java
+++ spring-dukes-payment/src/main/java/com/forest/payment/services/PaymentApplication.java
@@ -1,8 +1,11 @@
 package com.forest.payment.services;
 
+import javax.ws.rs.ApplicationPath;
+
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
 
+@ApplicationPath("/payment")
 public class PaymentApplication extends ResourceConfig {
 	/**
      * Register JAX-RS application components.
