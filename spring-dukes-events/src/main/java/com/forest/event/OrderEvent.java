c09829f378ebb8302242107ab67bf41b9d46cfe9
Andrej Burenin <andrejs.burenins@dukascopy.com>
2016-02-10 17:43:37 +0200

    improve order event

--- spring-dukes-events/src/main/java/com/forest/event/OrderEvent.java
+++ spring-dukes-events/src/main/java/com/forest/event/OrderEvent.java
@@ -3,8 +3,14 @@ package com.forest.event;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
 import org.springframework.context.ApplicationEvent;
 
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class OrderEvent<T> extends ApplicationEvent
 		implements Serializable {
 	
